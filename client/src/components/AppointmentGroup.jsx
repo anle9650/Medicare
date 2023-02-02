8b61b84782954ffdfb19a8c9ec803b77667c2f4b
anle9650 <andy.le@ucdenver.edu>
2023-02-01 21:16:42 -0700

    Update AppointmentGroup.jsx

--- client/src/components/AppointmentGroup.jsx
+++ client/src/components/AppointmentGroup.jsx
@@ -1,13 +1,18 @@
 import Appointment from "./Appointment";
 
 export default function AppointmentGroup(props) {
+
+  function formatHour(hour) {
+    return `${hour}:00 ${hour < 12 ? 'AM' : 'PM'}`;
+  }
+
   return (
-    <div className="grid grid-cols-6 gap-4">
-      <div className="flex items-start border-r-2 p-0">
-        <span>{props.startTime}</span>
+    <div className="grid grid-cols-12">
+      <div className="col-span-3 flex items-start border-r-2">
+        <span>{formatHour(props.startTime)}</span>
         <div className="bg-black rounded-full p-2 ml-auto mt-1 translate-x-2"></div>
       </div>
-      <div className="col-span-5 px-1 py-2">
+      <div className="col-span-9 pl-3 py-3">
           {props.appointments.map((appointment) => (
             <Appointment key={appointment.id} {...appointment} />
           ))}
