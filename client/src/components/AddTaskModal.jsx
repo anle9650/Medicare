eb7894ff2b4beb72c8f949e8c5b021aa09e71ac1
anle9650 <andy.le@ucdenver.edu>
2023-02-04 14:05:13 -0700

    create BaseModal

--- client/src/components/AddTaskModal.jsx
+++ client/src/components/AddTaskModal.jsx
@@ -1,10 +1,9 @@
-import { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { useState } from "react";
 import BaseDatepicker from "./BaseDatepicker";
+import BaseModal from "./BaseModal";
 
 export default function AddTaskModal(props) {
   const [task, setTask] = useState({ content: "", deadline: "" });
-  const cancelButtonRef = useRef(null);
 
   function updateTask(event) {
     const { name, value } = event.target;
@@ -28,103 +27,56 @@ export default function AddTaskModal(props) {
   }
 
   return (
-    <Transition.Root show={props.open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={props.onClose}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    <BaseModal open={props.open} onClose={props.onClose}>
+      <BaseModal.Header>Add Task</BaseModal.Header>
+      <BaseModal.Body>
+        <div className="flex flex-col justify-between h-[32rem] mt-2">
+          <form id="taskForm" onSubmit={addTask} data-testid="form">
+            <label
+              htmlFor="content"
+              className="block text-sm font-medium text-gray-700"
             >
-              <Dialog.Panel
-                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
-              >
-                <form
-                  className="flex flex-col justify-between h-[32rem]"
-                  onSubmit={addTask}
-                  data-testid="form"
-                >
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900"
-                      >
-                        Add Task
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <label
-                          htmlFor="content"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Description
-                        </label>
-                        <textarea
-                          id="content"
-                          name="content"
-                          value={task.content}
-                          onChange={updateTask}
-                          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          required
-                        />
-                        <label
-                          htmlFor="deadline"
-                          className="block text-sm font-medium text-gray-700 mt-3"
-                        >
-                          Deadline
-                        </label>
-                        <BaseDatepicker
-                          name="deadline"
-                          value={task.deadline}
-                          onSelect={(date) => setDeadline(date)}
-                          data-testid="datepicker"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    >
-                      Save
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={props.onClose}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </form>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+              Description
+            </label>
+            <textarea
+              id="content"
+              name="content"
+              value={task.content}
+              onChange={updateTask}
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="deadline"
+              className="block text-sm font-medium text-gray-700 mt-3"
+            >
+              Deadline
+            </label>
+            <BaseDatepicker
+              name="deadline"
+              value={task.deadline}
+              onSelect={(date) => setDeadline(date)}
+              data-testid="datepicker"
+            />
+          </form>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </BaseModal.Body>
+      <BaseModal.Footer>
+        <button
+          type="submit"
+          form="taskForm"
+          className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+        >
+          Save
+        </button>
+        <button
+          type="button"
+          className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          onClick={props.onClose}
+        >
+          Cancel
+        </button>
+      </BaseModal.Footer>
+    </BaseModal>
   );
 }
