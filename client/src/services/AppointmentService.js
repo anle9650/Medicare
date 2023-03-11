27902c905046c02159d297a05d05a498c8c0583d
anle9650 <andy.le@ucdenver.edu>
2023-03-11 11:49:42 -0700

    Update AppointmentService.js

--- client/src/services/AppointmentService.js
+++ client/src/services/AppointmentService.js
@@ -19,7 +19,7 @@ export async function addAppointmentRequest(appointment) {
 export async function updateAppointmentRequest(appointment) {
   const fields = {
     name: appointment.name,
-    patient_id: appointment.patient._id,
+    patient: appointment.patient._id,
     purpose: appointment.purpose,
     scheduledStart: appointment.scheduledStart,
     scheduledEnd: appointment.scheduledEnd,
